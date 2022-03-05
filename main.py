0c101b18dd6d366f443cc3f001fe8a1295bca587
iankoh93 <ian_koh93@hotmail.com>
2022-03-05 09:05:52 -0800

    Starting files for the NATO alphabet coding challenge

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-import pandas
-
-# new_dict ={new_key:new_value for item in list}
-# new_dict ={new_key:new_value for (key, value) in dict.items() if test}
-
-# new_list = [new_item for item in list]
-# new_list = [new_item for item in list if test]
-
 student_dict = {
-    "student": ["angela", "james", "Lily"],
+    "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
 }
 
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
-# # Loop thru data frame
-# for (key, value) in student_data_frame.items():
-#     print(value)
-
-# Loop through rows of a data frame
+#Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    if row.student == "angela":
-        print(row.score)
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
